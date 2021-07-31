01947e7db85d2875fc2c18a14128f4fe323fd5bd
Vincent OFFROY <vincent.offroy@engie.com>
2021-07-31 14:12:52 +0200

    let's get started

---
+++ app/src/main/java/test/dev/withings/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package test.dev.withings.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import test.dev.withings.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
